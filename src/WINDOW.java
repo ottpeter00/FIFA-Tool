7331989da43460ee9e43877ce6283ca591a192e4
ottpeter00 <44669369+ottpeter00@users.noreply.github.com>
2018-11-01 13:15:22 +0100

    GUI Fenster erstellt

---
+++ src/WINDOW.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class WINDOW {
+	
+	private JFrame fenster;
+	
+	public WINDOW() {
+		
+		fenster = new JFrame("FIFA Ultimate Tool (Alpha)");
+		fenster.setVisible(true);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenster.setBounds(100,100,600,800);
+		
+	}
+	
+}
