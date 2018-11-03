56c47e8c3a2b6410b07d0950dad3ec9851bfd3c0
Peter Ott <ottpeter00@gmail.com>
2018-11-03 15:32:38 +0100

    3.11.18 issue

--- src/WINDOW.java
+++ src/WINDOW.java
@@ -3,20 +3,23 @@ import javax.swing.*;
 public class WINDOW {
 	
 	private JFrame mainfenster;
-	private String aktuelleWL = "Aktuelle Weekend League";
+	private JLabel aktwllabel;
+	
 	
 	public WINDOW() {
 		
 		
 		
-		mainfenster = new JFrame("FIFA Ultimate Tool (Alpha)");
+		mainfenster = new JFrame("alpha");
 		mainfenster.setVisible(true);
 		mainfenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainfenster.setBounds(100,100,600,800);
 		
-		JLabel aktuelleWLlabel = new JLabel(aktuelleWL);
-		aktuelleWLlabel.setLocation(100,100); //issue
-		mainfenster.add(aktuelleWLlabel);
+		aktwllabel = new JLabel("Aktuell");
+		aktwllabel.setLocation(50,50); //issue
+		mainfenster.add(aktwllabel);
+		
+		
 		
 	}
 	
